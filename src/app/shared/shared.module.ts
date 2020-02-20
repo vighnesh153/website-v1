864595f49cf4ecc7cecf1ad5b54c59fb20bc8d0b
vighnesh153 <vighnesh.raut13@gmail.com>
2020-02-20 21:32:29 +0530

    re-work on navbar to make it minimilistic and refactored out unnecessary logic

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HamburgerComponent } from './components/hamburger/hamburger.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +12,8 @@ import { HamburgerComponent } from './components/hamburger/hamburger.component';
     HamburgerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     NavBarComponent
