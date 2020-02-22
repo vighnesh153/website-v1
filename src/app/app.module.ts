16591ce196c177b9e7ea8cd4d0d96029f73fbef6
vighnesh153 <vighnesh.raut13@gmail.com>
2020-02-22 11:40:20 +0530

    refactored the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
-import { ContactComponent } from './components/contact/contact.component';
+import { ConnectComponent } from './components/connect/connect.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    AboutComponent,
+    HomeComponent,
     ExperienceComponent,
-    ContactComponent,
-    NotFoundComponent
+    ConnectComponent,
+    NotFoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     SharedModule
   ],
   providers: [],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  resumeUrl: 'https://drive.google.com/open?id=1u_8oDHemUAYZdO99AGEYX8iqRrl3lNGO',
+  github: {
+    repoInfoUrl: 'https://api.github.com/repos/vighnesh153-canvas/angular-client/branches/master'
+  }
 };
